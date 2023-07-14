4c2508f108e7eee6d4868e6a3dcd245a15f0660c
Ольга <olga@MacBook-Air-Olga.local>
2023-07-14 03:34:36 +0300

    color legend

--- src/legend.tsx
+++ src/legend.tsx
@@ -10,7 +10,7 @@ function Legend(props: TLegend) {
     return (
           <div className = {LegendStyle.line} style = {{opacity: props.active ? "1" : "0.6"}}>
               <div className = {LegendStyle.square} style = {{background: props.color}}></div>
-              <span className = {LegendStyle.title} style = {{color: props.color, textDecorationLine: props.active ? "underline" : "none", textDecorationColor: props.color}}>{props.text+" Сегмент"}</span>
+              <span className = {LegendStyle.title} style = {{textDecorationLine: props.active ? "underline" : "none", textDecorationColor: props.color}}>{props.text+" Сегмент"}</span>
           </div>
     )}
 export default Legend;
